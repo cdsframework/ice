dbf398807d7f1d26b3c3ebe96ba559a9cf3ccd1d
daryl <devnull@localhost>
2017-11-25 23:12:44 -0500

    Fix to DTP exception rule 1- mistakenly marked 4th dose complete based on age rather than date of shot DSL fix

--- ice3/opencds-ice-service/src/main/groovy/org/cdsframework/ice/util/IceClient.groovy
+++ ice3/opencds-ice-service/src/main/groovy/org/cdsframework/ice/util/IceClient.groovy
@@ -19,7 +19,7 @@ class IceClient {
 
 		def params = [
 			kmEvaluationRequest:[scopingEntityId: 'org.nyc.cir', businessId: 'ICE', version: '1.0.0'],
-			specifiedTime: '2017-11-17'
+			specifiedTime: '2023-01-01'
 		]
 		def params1_0_0 = [
 			kmEvaluationRequest:[scopingEntityId: 'org.nyc.cir', businessId: 'ICE', version: '1.0.0'],
