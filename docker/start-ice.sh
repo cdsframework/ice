f03176db0d20de693dc47e047033f2d97e21b071
Samuel Nicolary <sam@nicolary.org>
2020-03-18 17:36:15 -0700

    shell script fixes

--- docker/start-ice.sh
+++ docker/start-ice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ -v $DEBUG ]]; then
+if [[ -v DEBUG ]]; then
     echo "DEBUG is set to: $DEBUG"
     if [ "$DEBUG" == "Y" ]; then
         echo "Turning debug on..."
@@ -12,48 +12,48 @@ PRE_EVALUATE_UUID=$(cat /proc/sys/kernel/random/uuid)
 echo "Setting system property preEvaluateUuid to: $PRE_EVALUATE_UUID"
 export JAVA_OPTS="$JAVA_OPTS -DpreEvaluateUuid=$PRE_EVALUATE_UUID"
 
-if [[ -v $PRE_EVALUATE_HOOK_TYPE ]]
+if [[ -v PRE_EVALUATE_HOOK_TYPE ]]
 then
     echo "Setting system property preEvaluateHookType to: $PRE_EVALUATE_HOOK_TYPE"
     export JAVA_OPTS="$JAVA_OPTS -DpreEvaluateHookType=$PRE_EVALUATE_HOOK_TYPE"
 fi
 
-if [[ -v $PRE_EVALUATE_HOOK_URI ]]
+if [[ -v PRE_EVALUATE_HOOK_URI ]]
 then
     echo "Setting system property preEvaluateHookUri to: $PRE_EVALUATE_HOOK_URI"
     export JAVA_OPTS="$JAVA_OPTS -DpreEvaluateHookUri=$PRE_EVALUATE_HOOK_URI"
 fi
 
-if [[ -v $TZ ]]; then
+if [[ -v TZ ]]; then
     echo "TZ is set to: $TZ"
     export JAVA_OPTS="$JAVA_OPS -Duser.timezone=$TZ"
 fi
 
-if [[ -v $KM_THREADS ]]
+if [[ -v KM_THREADS ]]
 then
     echo "Setting km.threads in opencds.properties to: $KM_THREADS"
     sed -i "s/km.threads=.*/km.threads=$KM_THREADS/" .opencds/opencds.properties
 fi
 
-if [[ -v $OUTPUT_EARLIEST_OVERDUE_DATES ]]
+if [[ -v OUTPUT_EARLIEST_OVERDUE_DATES ]]
 then
     echo "Setting output_earliest_and_overdue_dates in ice.properties to: $OUTPUT_EARLIEST_OVERDUE_DATES"
     sed -i "s/output_earliest_and_overdue_dates=.*/output_earliest_and_overdue_dates=$OUTPUT_EARLIEST_OVERDUE_DATES/" /usr/local/tomcat/webapps/opencds-decision-support-service/WEB-INF/classes/ice.properties
 fi
 
-if [[ -v $ENABLE_DOSE_OVERRIDE_FEATURE ]]
+if [[ -v ENABLE_DOSE_OVERRIDE_FEATURE ]]
 then
     echo "Setting enable_dose_override_feature in ice.properties to: $ENABLE_DOSE_OVERRIDE_FEATURE"
     sed -i "s/enable_dose_override_feature=.*/enable_dose_override_feature=$ENABLE_DOSE_OVERRIDE_FEATURE/" /usr/local/tomcat/webapps/opencds-decision-support-service/WEB-INF/classes/ice.properties
 fi
 
-if [[ -v $OUTPUT_SUPPLEMENTAL_TEXT ]]
+if [[ -v OUTPUT_SUPPLEMENTAL_TEXT ]]
 then
     echo "Setting output_supplemental_text in ice.properties to: $OUTPUT_SUPPLEMENTAL_TEXT"
     sed -i "s/output_supplemental_text=.*/output_supplemental_text=$OUTPUT_SUPPLEMENTAL_TEXT/" /usr/local/tomcat/webapps/opencds-decision-support-service/WEB-INF/classes/ice.properties
 fi
 
-if [[ -v $REMOTE_CONFIG_ENABLED ]]; then
+if [[ -v REMOTE_CONFIG_ENABLED ]]; then
     echo "REMOTE_CONFIG_ENABLED is set to: $REMOTE_CONFIG_ENABLED"
     if [ "$REMOTE_CONFIG_ENABLED" == "Y" ]; then
         echo "Turning remote configuration on - make sure you have the user and password set via the REMOTE_CONFIG_USER and REMOTE_CONFIG_PASSWORD environment variables."
@@ -61,18 +61,18 @@ if [[ -v $REMOTE_CONFIG_ENABLED ]]; then
     fi
 fi
 
-if [[ -v $REMOTE_CONFIG_USER ]]; then
+if [[ -v REMOTE_CONFIG_USER ]]; then
     echo "Setting username in sec.xml to: $REMOTE_CONFIG_USER"
     sed -i "s/<username>.*<\/username>/<username>$REMOTE_CONFIG_USER<\/username>/" .opencds/sec.xml
     unset REMOTE_CONFIG_USER
 fi
 
-if [[ -v $REMOTE_CONFIG_PASSWORD ]]; then
+if [[ -v REMOTE_CONFIG_PASSWORD ]]; then
     echo "Setting password in sec.xml to the configured value."
     sed -i "s/<password>.*<\/password>/<password>$REMOTE_CONFIG_PASSWORD<\/password>/" .opencds/sec.xml
     unset REMOTE_CONFIG_PASSWORD
 fi
 
-echo $JAVA_OPTS
+echo JAVA_OPTS=$JAVA_OPTS
 
 . $@
